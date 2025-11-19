a817d960e614a87ac8b6cc85848a2a775a6a06ee
Alejandro Martinez <jmartinez75@udi.edu.co>
2025-11-18 22:08:40 -0500

    juego al 90%

--- scripts/enemies/enemy_spawns_config.gd
+++ scripts/enemies/enemy_spawns_config.gd
@@ -10,18 +10,46 @@ static func get_enemy_spawns() -> Array[Dictionary]:
 	spawns.append({
 		"scene": centinela,
 		"room_path": "res://scenes/map/level1/pasillos.tscn",
-		"position": Vector2(882, 391),
+		"position": Vector2(499, 391),
 		"enemy_id": "centinela_pasillo_01"
 	})
 	
 	spawns.append({
 		"scene": centinela,
 		"room_path": "res://scenes/map/level1/pasillos.tscn",
-		"position": Vector2(1267, 390),
-		"enemy_id": "centinela_recepcion_02"
+		"position": Vector2(1267, 391),
+		"enemy_id": "centinela_pasillo_02"
 	})
-
-
+	
+	spawns.append({
+		"scene": centinela,
+		"room_path": "res://scenes/map/level1/pasillos.tscn",
+		"position": Vector2(499, 7),
+		"enemy_id": "centinela_pasillo_03"
+	})
+	
+	spawns.append({
+		"scene": centinela,
+		"room_path": "res://scenes/map/level1/pasillos.tscn",
+		"position": Vector2(1267, 7),
+		"enemy_id": "centinela_pasillo_04"
+	})
+	
+	# SALONES
+	spawns.append({
+		"scene": centinela,
+		"room_path": "res://scenes/map/level1/salon_101.tscn",
+		"position": Vector2(39, 125),
+		"enemy_id": "centinela_salon101"
+	})
+	
+	spawns.append({
+		"scene": centinela,
+		"room_path": "res://scenes/map/level1/salon_202.tscn",
+		"position": Vector2(53, 148),
+		"enemy_id": "centinela_salon202"
+	})
+	
 	# ... más spawns
 	
 	return spawns
